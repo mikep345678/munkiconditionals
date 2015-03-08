12b5a0a8de3a24e09250422c12756b839f448beb
Mike Pullen <mike@thepullen.net>
2015-03-08 16:14:45 -0500

    added initial munkiconditionals-ADOD.sh

--- munkiconditionals-ADOD.sh
+++ munkiconditionals-ADOD.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SCRIPT_VERSION="20150119.01"
+SCRIPT_VERSION="20150121.01"
 SCRIPT_STAMP="munkiconditionals-ADOD"
 
 
@@ -21,6 +21,9 @@ ldapserver=$( dscl localhost -read /Search | grep -m 1 LDAP )
 # define Active Directory tree
 AD="/Active Directory/AD/All Domains"
 
+# find current machine's FQDN
+current_machine_fqdn= dscl $ldapserver -read /Computers/$
+
 # Make sure we're outputting our information to "ConditionalItems.plist" (plist is left off since defaults requires this)
 plist_loc="$managedinstalldir/ConditionalItems"
 
@@ -30,7 +33,7 @@ appgroups-OD=$( dscl $ldapserver -list /ComputerGroups | grep "munkiapp" )
 appgroups-AD=$( dscl "$AD" -list Groups | grep "munkiapp" )
 
 # OD
-# Build list of assigned applications by querying OD group membership of current computer 
+# Build list of assigned applications by querying OD group membership of current computer
 for app in $appgroups-OD
 do
 	if $dsgu -q 1 -o checkmember -t computer -currentHost 1 -g $app; then
@@ -40,7 +43,22 @@ do
 done
 
 # AD
-# Build ist
+# Build list of munkiapp groups in AD
+appgroups-AD=$( dscl "$AD" -list Groups | grep "munkiapp" )
+reqsubstr="_comp_"
+for i in $Groups; do
+	if [ -z "${i##*$reqsubstr*}" ]; then
+		tmp=${i#*=}
+		group=${tmp%%,*}
+		echo $group
+	fi
+done
+
+# gather lists of (nested) groups of which current machine is a member
+#memberof=$( ldapsearch -D 'AD\LDAPDirectory' -w 'LDAPsearch' -x -H ldap://ad.barabooschools.net -LLL -u -t -b "DC=ad,DC=barabooschools,DC=net" -s sub memberOf:1.2.840.113556.1.4.1941:=CN=odtest-c1885,CN=Computers,DC=ad,DC=barabooschools,DC=net )
+memberof=$( ldapsearch -D 'AD\LDAPDirectory' -w 'LDAPsearch' \
+	-x -H ldap://ad.barabooschools.net -LLL -u -t -b "DC=ad,DC=barabooschools,DC=net" -s sub \
+	memberOf:1.2.840.113556.1.4.1941:=$current_machine_fqdn )
 
 # Note the key "assignedapps" which becomes the condition that you would use in a predicate statement
 defaults write "$plist_loc" "assignedapps" -array "${assignedapps[@]}"
